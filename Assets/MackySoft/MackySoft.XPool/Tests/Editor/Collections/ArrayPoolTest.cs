b18e4a4ce6d211d37fffcfa972268454877cd3e0
mackysoft <mackysoft0129@gmail.com>
2022-01-30 14:58:36 +0900

    Add allocate test

--- Assets/MackySoft/MackySoft.XPool/Tests/Editor/Collections/ArrayPoolTest.cs
+++ Assets/MackySoft/MackySoft.XPool/Tests/Editor/Collections/ArrayPoolTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
 using UnityEngine;
+using UnityEngine.TestTools.Constraints;
+using Is = UnityEngine.TestTools.Constraints.Is;
 
 namespace MackySoft.XPool.Collections.Tests {
 	public class ArrayPoolTest {
@@ -21,6 +23,19 @@ namespace MackySoft.XPool.Collections.Tests {
 		[Test]
 		public void ReturnedArrayLengthIsNextPowerOfTwo ([Random(0,1048576,100)] int size) {
 			Assert.AreEqual(Mathf.NextPowerOfTwo(size),m_Pool.Rent(size).Length);
+		}
+
+		[Test]
+		public void RentDoesNotAllocate () {
+			WarmUp(m_Pool,5);
+
+			Assert.That(() => {
+				m_Pool.Rent(5);
+			},Is.Not.AllocatingGCMemory());
+		}
+
+		static void WarmUp<T> (ArrayPool<T> pool,int size) {
+			pool.Return(pool.Rent(size));
 		}
 
 	}
