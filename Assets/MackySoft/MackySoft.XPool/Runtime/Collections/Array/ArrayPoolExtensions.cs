64810ff4792f77e3c03fe1eff5a3d422deb1c772
mackysoft <mackysoft0129@gmail.com>
2022-01-30 18:01:32 +0900

    Update ArrayPoolExtensions.cs

--- Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/ArrayPoolExtensions.cs
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/ArrayPoolExtensions.cs
@@ -9,7 +9,15 @@ namespace MackySoft.XPool.Collections {
 		/// <para> The array length is not always accurate. </para>
 		/// </summary>
 		public static T[] ToArrayFromPool<T> (this IEnumerable<T> source) {
-			return ToArrayFromPool(source,out _);
+			return ToArrayFromPool(source,ArrayPool<T>.Shared,out _);
+		}
+
+		/// <summary>
+		/// <para> Convert enumerable to array. Array are returned from <see cref="ArrayPool{T}"/>. </para>
+		/// <para> The array length is not always accurate. </para>
+		/// </summary>
+		public static T[] ToArrayFromPool<T> (this IEnumerable<T> source,ArrayPool<T> pool) {
+			return ToArrayFromPool(source,pool,out _);
 		}
 
 		/// <summary>
@@ -18,29 +26,41 @@ namespace MackySoft.XPool.Collections {
 		/// </summary>
 		/// <param name="count"> Number of elements in source. </param>
 		public static T[] ToArrayFromPool<T> (this IEnumerable<T> source,out int count) {
+			return ToArrayFromPool(source,ArrayPool<T>.Shared,out count);
+		}
+
+		/// <summary>
+		/// <para> Convert enumerable to array. Array are returned from <see cref="ArrayPool{T}"/>. </para>
+		/// <para> The array length is not always accurate. </para>
+		/// </summary>
+		/// <param name="count"> Number of elements in source. </param>
+		public static T[] ToArrayFromPool<T> (this IEnumerable<T> source,ArrayPool<T> pool,out int count) {
 			if (source == null) {
 				throw new ArgumentNullException(nameof(source));
+			}
+			if (pool == null) {
+				throw new ArgumentNullException(nameof(pool));
 			}
 
 			// Tries to cast source to the collection interfaces.
 			if (source is IList<T> list) {
 				count = list.Count;
-				return ToArrayFromPoolInternal(list);
+				return ToArrayFromPoolInternal(list,pool);
 			}
 			if (source is IReadOnlyList<T> readonlyList) {
 				count = readonlyList.Count;
-				return ToArrayFromPoolInternal(readonlyList);
+				return ToArrayFromPoolInternal(readonlyList,pool);
 			}
 			if (source is ICollection<T> collection) {
 				count = collection.Count;
-				return ToArrayFromPoolInternal(collection);
+				return ToArrayFromPoolInternal(collection,pool);
 			}
 			if (source is IReadOnlyCollection<T> readonlyCollection) {
 				count = readonlyCollection.Count;
-				return ToArrayFromPoolInternal(readonlyCollection);
+				return ToArrayFromPoolInternal(readonlyCollection,pool);
 			}
 
-			T[] array = ArrayPool<T>.Shared.Rent(32);
+			T[] array = pool.Rent(32);
 			count = 0;
 			foreach (T item in source) {
 				ArrayPoolUtility.EnsureCapacity(ref array,count);
@@ -50,22 +70,22 @@ namespace MackySoft.XPool.Collections {
 			return array;
 		}
 
-		static T[] ToArrayFromPoolInternal<T> (IReadOnlyList<T> source) {
-			T[] array = ArrayPool<T>.Shared.Rent(source.Count);
+		static T[] ToArrayFromPoolInternal<T> (IReadOnlyList<T> source,ArrayPool<T> pool) {
+			T[] array = pool.Rent(source.Count);
 			for (int i = 0;source.Count > i;i++) {
 				array[i] = source[i];
 			}
 			return array;
 		}
 
-		static T[] ToArrayFromPoolInternal<T> (IList<T> source) {
-			T[] array = ArrayPool<T>.Shared.Rent(source.Count);
+		static T[] ToArrayFromPoolInternal<T> (IList<T> source,ArrayPool<T> pool) {
+			T[] array = pool.Rent(source.Count);
 			source.CopyTo(array,0);
 			return array;
 		}
 
-		static T[] ToArrayFromPoolInternal<T> (IReadOnlyCollection<T> source) {
-			T[] array = ArrayPool<T>.Shared.Rent(source.Count);
+		static T[] ToArrayFromPoolInternal<T> (IReadOnlyCollection<T> source,ArrayPool<T> pool) {
+			T[] array = pool.Rent(source.Count);
 			int i = 0;
 			foreach (T item in source) {
 				array[i] = item;
@@ -74,8 +94,8 @@ namespace MackySoft.XPool.Collections {
 			return array;
 		}
 
-		static T[] ToArrayFromPoolInternal<T> (ICollection<T> source) {
-			T[] array = ArrayPool<T>.Shared.Rent(source.Count);
+		static T[] ToArrayFromPoolInternal<T> (ICollection<T> source,ArrayPool<T> pool) {
+			T[] array = pool.Rent(source.Count);
 			source.CopyTo(array,0);
 			return array;
 		}
