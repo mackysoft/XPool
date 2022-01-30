87fb82a7088fe383c61c6e3664f6d4cb3eda15b8
mackysoft <mackysoft0129@gmail.com>
2022-01-30 16:18:37 +0900

    Separate `TemporaryArray` file

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/TemporaryArray.Create.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace MackySoft.XPool.Collections
+{
+	public partial struct TemporaryArray<T> {
+
+		/// <summary>
+		/// Create a temporary array of the specified length.
+		/// </summary>
+		public static TemporaryArray<T> Create (int length) {
+			return new TemporaryArray<T>(ArrayPool<T>.Shared.Rent(length),length);
+		}
+
+		/// <summary>
+		/// <para> Create a temporary array with a length of 0. </para>
+		/// <para> The length can be increased by using the <see cref="Add(T)"/>. </para>
+		/// </summary>
+		/// <param name="prepare"> Length of the internal array to be prepared. </param>
+		public static TemporaryArray<T> CreateAsList (int prepare) {
+			return new TemporaryArray<T>(ArrayPool<T>.Shared.Rent(prepare),0);
+		}
+
+		/// <summary>
+		/// Create a temporary array from the elements of <see cref="IEnumerable{T}"/>.
+		/// </summary>
+		/// <exception cref="ArgumentNullException"></exception>
+		public static TemporaryArray<T> From (IEnumerable<T> source) {
+			if (source == null) {
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			T[] array = source.ToArrayFromPool(out int count);
+			return new TemporaryArray<T>(array,count);
+		}
+
+		public static TemporaryArray<T> From (TemporaryArray<T> source) {
+			var result = Create(source.Length);
+			for (int i = 0;source.Length > i;i++) {
+				result[i] = source[i];
+			}
+			return result;
+		}
+
+	}
+}
--- Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/TemporaryArray.cs
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/TemporaryArray.cs
@@ -4,49 +4,13 @@ using System.Collections.Generic;
 using MackySoft.XPool.Collections.Internal;
 
 namespace MackySoft.XPool.Collections {
+
 	/// <summary>
 	/// <para> Temporary array without allocation. </para>
 	/// <para> This struct use <see cref="ArrayPool{T}"/> internally to avoid allocation and can be used just like a normal array. </para>
 	/// <para> After using it, please call the Dispose(). </para>
 	/// </summary>
-	public struct TemporaryArray<T> : IEnumerable<T>, IDisposable {
-
-		/// <summary>
-		/// Create a temporary array of the specified length.
-		/// </summary>
-		public static TemporaryArray<T> Create (int length) {
-			return new TemporaryArray<T>(ArrayPool<T>.Shared.Rent(length),length);
-		}
-
-		/// <summary>
-		/// <para> Create a temporary array with a length of 0. </para>
-		/// <para> The length can be increased by using the <see cref="Add(T)"/>. </para>
-		/// </summary>
-		/// <param name="prepare"> Length of the internal array to be prepared. </param>
-		public static TemporaryArray<T> CreateAsList (int prepare) {
-			return new TemporaryArray<T>(ArrayPool<T>.Shared.Rent(prepare),0);
-		}
-
-		/// <summary>
-		/// Create a temporary array from the elements of <see cref="IEnumerable{T}"/>.
-		/// </summary>
-		/// <exception cref="ArgumentNullException"></exception>
-		public static TemporaryArray<T> From (IEnumerable<T> source) {
-			if (source == null) {
-				throw new ArgumentNullException(nameof(source));
-			}
-
-			T[] array = source.ToArrayFromPool(out int count);
-			return new TemporaryArray<T>(array,count);
-		}
-
-		public static TemporaryArray<T> From (TemporaryArray<T> source) {
-			var result = Create(source.Length);
-			for (int i = 0;source.Length > i;i++) {
-				result[i] = source[i];
-			}
-			return result;
-		}
+	public partial struct TemporaryArray<T> : IEnumerable<T>, IDisposable {
 
 		T[] m_Array;
 		int m_Length;
