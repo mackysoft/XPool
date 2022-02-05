1b520e387c2f35a9a4abc79f3f1f7fc358387be0
mackysoft <mackysoft0129@gmail.com>
2022-02-05 20:37:09 +0900

    Fix EnsureCapacity

--- Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/ArrayPoolUtility.cs
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Collections/Array/ArrayPoolUtility.cs
@@ -8,7 +8,7 @@ namespace MackySoft.XPool.Collections {
 
 		public static void EnsureCapacity<T> (ref T[] array,int index,ArrayPool<T> pool) {
 			if (array.Length <= index) {
-				int newSize = array.Length * 2;
+				int newSize = (array.Length != 0) ? array.Length * 2 : kMinArraySize;
 				T[] newArray = pool.Rent((index < newSize) ? newSize : (index * 2));
 				Array.Copy(array,0,newArray,0,array.Length);
 
