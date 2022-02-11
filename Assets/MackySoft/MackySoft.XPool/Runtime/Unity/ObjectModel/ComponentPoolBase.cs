ae7231be936cdb618e2eeffcec0a22aa6ac06e4f
mackysoft <mackysoft0129@gmail.com>
2022-02-12 00:50:38 +0900

    Add ObjectModel & Update gitignore

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/ObjectModel/ComponentPoolBase.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace MackySoft.XPool.Unity.ObjectModel {
+	public abstract class ComponentPoolBase<T> : UnityObjectPoolBase<T>, IHierarchicalUnityObjectPool<T> where T : Component {
+
+		public T Rent (Vector3 position,Quaternion rotation,Transform parent = null) {
+			T instance = GetPooledInstance();
+			if (instance != null) {
+				Transform transform = parent.transform;
+				transform.SetParent(parent);
+				transform.SetPositionAndRotation(position,rotation);
+			}
+			else {
+				instance = UnityEngine.Object.Instantiate(m_Original,position,rotation,parent);
+				OnInstantiate(instance);
+			}
+			OnRent(instance);
+			return instance;
+		}
+
+		public T Rent (Transform parent,bool worldPositionStays) {
+			T instance = GetPooledInstance();
+			if (instance != null) {
+				instance.transform.SetParent(parent,worldPositionStays);
+			}
+			else {
+				instance = UnityEngine.Object.Instantiate(m_Original,parent,worldPositionStays);
+				OnInstantiate(instance);
+			}
+			OnRent(instance);
+			return instance;
+		}
+
+	}
+}
---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/ObjectModel/UnityObjectPoolBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace MackySoft.XPool.Unity.ObjectModel {
+	public abstract class UnityObjectPoolBase<T> : IUnityObjectPool<T> where T : UnityObject {
+		[SerializeField]
+		protected T m_Original;
+
+		[SerializeField]
+		int m_Capacity;
+
+		Queue<T> m_Pool;
+
+		public T Original => m_Original;
+
+		public T Rent () {
+			T instance = GetPooledInstance();
+			if (instance == null) {
+				instance = UnityObject.Instantiate(m_Original);
+			}
+			OnRent(instance);
+			return instance;
+		}
+
+		public void Return (T instance) {
+			if (instance == null) {
+				throw new ArgumentNullException(nameof(instance));
+			}
+			if (m_Pool.Count == m_Capacity) {
+				OnRelease(instance);
+				return;
+			}
+			m_Pool.Enqueue(instance);
+			OnReturn(instance);
+		}
+
+		public void ReleaseInstances (int keep) {
+			if ((keep < 0) || (keep > m_Capacity)) {
+				throw new ArgumentOutOfRangeException(nameof(keep));
+			}
+
+			if (keep != 0) {
+				for (int i = m_Pool.Count - keep;i > 0;i--) {
+					T instance = m_Pool.Dequeue();
+					if (instance != null) {
+						OnRelease(instance);
+					}
+				}
+			}
+			else {
+				while (m_Pool.Count > 0) {
+					T instance = m_Pool.Dequeue();
+					if (instance != null) {
+						OnRelease(instance);
+					}
+				}
+			}
+		}
+
+		protected T GetPooledInstance () {
+			T instance = null;
+			while ((m_Pool.Count > 0) && (instance == null)) {
+				instance = m_Pool.Dequeue();
+			}
+			return instance;
+		}
+
+		protected abstract void OnInstantiate (T instance);
+
+		protected abstract void OnRent (T instance);
+		protected abstract void OnReturn (T instance);
+		protected abstract void OnRelease (T instance);
+	}
+}
