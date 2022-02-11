9a83dd9e2f4ff628e4da4dba2652591ea70e351f
mackysoft <mackysoft0129@gmail.com>
2022-02-11 20:25:58 +0900

    Add IUnityObjectPool interfaces

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/IUnityObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace MackySoft.XPool.Unity {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IUnityObjectPool<T> : IPool<T> where T : UnityObject {
+		T Original { get; }
+	}
+
+	/// <summary>
+	/// Interface that provides rent method for GameObject or components.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IHierarchicalUnityObjectPool<T> : IUnityObjectPool<T> where T : UnityObject {
+		T Rent (Vector3 position,Quaternion rotation,Transform parent = null);
+		T Rent (Transform parent,bool worldPositionStays);
+	}
+}
