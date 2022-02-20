5b7c58411eaf7a3c22a83c9fc71c12f3a167d3d8
mackysoft <mackysoft0129@gmail.com>
2022-02-20 17:05:46 +0900

    Add `Error`
    Add `Error.Empty` method

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Internal/Error.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace MackySoft.XPool.Internal {
+    internal static class Error {
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static InvalidOperationException Empty () {
+			return new InvalidOperationException("Collection is empty.");
+		}
+
+	}
+}
