e903b0c7a15db73a2fcd6cf5ba89d042e58629e0
mackysoft <mackysoft0129@gmail.com>
2022-02-26 20:50:58 +0900

    Set default function for `UnityObjectPool.m_OnRelease`

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/DefaultFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace MackySoft.XPool.Unity {
+	public static class DefaultFunctions {
+
+		public static Action<T> Destroy<T> () where T : UnityObject {
+			return CachedFunctions<T>.Destroy;
+		}
+
+		static class CachedFunctions<T> where T : UnityObject {
+
+			public static readonly Action<T> Destroy = instance => {
+#if UNITY_EDITOR
+				if (Application.isPlaying) {
+					UnityObject.Destroy(instance);
+				}
+				else {
+					UnityObject.DestroyImmediate(instance);
+				}
+#else
+				UnityObject.Destroy(instance);
+#endif
+			};
+		}
+
+	}
+}
