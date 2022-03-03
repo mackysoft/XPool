eaa71d592e38ce348a29f715f7d9b01a402c2ebe
mackysoft <mackysoft0129@gmail.com>
2022-03-03 23:18:50 +0900

    Add `TimerTicker`

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Timers/TimerTicker.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using MackySoft.XPool.Timers;
+using UnityEngine;
+
+namespace MackySoft.XPool.Timer {
+	public class TimerTicker : MonoBehaviour {
+
+		public static TimerTicker Instance {
+			get {
+				if (s_Instance == null) {
+					var go = new GameObject("[XPool] TimerTicker");
+					s_Instance = go.AddComponent<TimerTicker>();
+					DontDestroyOnLoad(go);
+				}
+				return s_Instance;
+			}
+		}
+
+		static TimerTicker s_Instance;
+
+		readonly List<ITimer> m_Timers = new List<ITimer>();
+
+		void Awake () {
+			if (s_Instance == null) {
+				s_Instance = this;
+			}
+		}
+
+		void OnDestroy () {
+			if (s_Instance == this) {
+				s_Instance = null;
+			}
+		}
+
+		void Update () {
+			float deltaTime = Time.deltaTime;
+			for (int i = 0;i < m_Timers.Count;i++) {
+				m_Timers[i].Tick(deltaTime);
+			}
+		}
+
+		public void Register (ITimer timer) {
+			m_Timers.Add(timer);
+		}
+
+		public void Unregister (ITimer timer) {
+			m_Timers.Remove(timer);
+		}
+
+		public IDisposable Subscribe (ITimer timer) {
+			return new TimerRegistration(timer,this);
+		}
+
+		class TimerRegistration : IDisposable {
+
+			readonly ITimer m_Timer;
+			readonly TimerTicker m_TimerTicker;
+			bool m_IsDisposed;
+
+			public TimerRegistration (ITimer timer,TimerTicker timerTicker) {
+				m_Timer = timer;
+				m_TimerTicker = timerTicker;
+
+				m_TimerTicker.Register(timer);
+			}
+
+			public void Dispose () {
+				if (m_IsDisposed) {
+					return;
+				}
+				m_IsDisposed = true;
+				m_TimerTicker.Unregister(m_Timer);
+			}
+		}
+	}
+}
