a643ec40f180e20f5ac5ee5255f5db7dacdfafac
mackysoft <mackysoft0129@gmail.com>
2022-03-05 22:55:26 +0900

    Add `IUnityObjectFactory`

---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/IUnityObjectFactory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace MackySoft.XPool.Unity {
+	public interface IUnityObjectFactory<out T> where T : UnityObject {
+		T Factory ();
+		T Factory (Vector3 position,Quaternion rotation,Transform parent = null);
+		T Factory (Transform parent,bool worldPositionStays);
+	}
+}
---
+++ Assets/MackySoft/MackySoft.XPool/Runtime/Unity/UnityObjectFactory/DefaultUnityObjectFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using MackySoft.XPool.Internal;
+using UnityObject = UnityEngine.Object;
+
+namespace MackySoft.XPool.Unity {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	[Serializable]
+	public sealed class DefaultUnityObjectFactory<T> : IUnityObjectFactory<T> where T : UnityObject {
+
+		[Tooltip("The original object from which the pool will instantiate a new instance.")]
+		[SerializeField]
+		T m_Original;
+
+		/// <summary>
+		/// The original object from which the pool will instantiate a new instance.
+		/// </summary>
+		public T Original => m_Original;
+
+		public DefaultUnityObjectFactory () {
+		}
+
+		public DefaultUnityObjectFactory (T original) {
+			m_Original = (original != null) ? original : throw Error.ArgumentNullException(nameof(original));
+		}
+
+		public T Factory () {
+			return UnityObject.Instantiate(m_Original);
+		}
+
+		public T Factory (Vector3 position,Quaternion rotation,Transform parent = null) {
+			return UnityObject.Instantiate(m_Original,position,rotation,parent);
+		}
+
+		public T Factory (Transform parent,bool worldPositionStays) {
+			return UnityObject.Instantiate(m_Original,parent,worldPositionStays);
+		}
+	}
+}
